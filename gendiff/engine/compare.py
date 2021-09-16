d981e735652f3ccb4422e62ac15fb65839dcc692
Nidenai <30Dk81Yz>
2021-09-16 19:43:49 +0700

    some fixes and go to step 5 (fix 2)

--- gendiff/engine/compare.py
+++ gendiff/engine/compare.py
@@ -1,36 +1,42 @@
+import argparse
 import json
 
 
+def run():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', metavar='FORMAT', help='set format of output')
+    parsing(parser.parse_args())
+
+
+def parsing(args):
+    print(generate_diff(args.first_file, args.second_file))
+
+
 def generate_diff(file_one, file_two):
-    origin = load_file(file_one)
-    modified = load_file(file_two)
-    origin_keys = list(origin.keys())
-    modified_keys = list(modified.keys())
-    all_keys = sorted(set(origin_keys + modified_keys))
+    insert_data = is_json(file_one, file_two)
+    one, two = insert_data
+    one_keys = list(one.keys())
+    two_keys = list(two.keys())
+    all_keys = sorted(set(one_keys + two_keys))
     res = ['{']
     for key in all_keys:
-        if key not in origin_keys:
-            res.append('+ {}: {}'.format(key, modified[key]))
-        elif key not in modified_keys:
-            res.append('- {}: {}'.format(key, origin[key]))
+        if key not in one_keys:
+            res.append('+ {}: {}'.format(key, two[key]))
+        elif key not in two_keys:
+            res.append('- {}: {}'.format(key, one[key]))
         else:
-            if origin[key] == modified[key]:
-                res.append('  {}: {}'.format(key, origin[key]))
+            if one[key] == two[key]:
+                res.append('  {}: {}'.format(key, one[key]))
             else:
-                res.append('- {}: {}'.format(key, origin[key]))
-                res.append('+ {}: {}'.format(key, modified[key]))
+                res.append('- {}: {}'.format(key, one[key]))
+                res.append('+ {}: {}'.format(key, two[key]))
     res.append('}')
     return '\n'.join(res)
 
 
-def load_file(file_path):
-    return json.load(open(file_path))
-
-
-def is_json(file1, file2)
-    return file1.endswith(".json") and file2.endswith(".json")
-
-
-def is_yaml(file1, file2):
-    return file1.endswith((".yaml", ".yml")) and \
-        file2.endswith((".yaml", ".yml"))
+def is_json(file_one, file_two):
+    one = json.load(open(file_one))
+    two = json.load(open(file_two))
+    return (one, two)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,8 @@
-#!/usr/bin/env python
-
-import argparse
-from gendiff.engine.compare import generate_diff
+from gendiff.engine.compare import run
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', metavar='FORMAT', help='set format of output')
-    parsing(parser.parse_args())
-
-
-def parsing(args):
-    print(generate_diff(args.first_file, args.second_file))
+    run()
 
 
 if __name__ == '__main__':
