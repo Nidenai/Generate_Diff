097f54df579b3417f238f2bac61138a3616fada5
Nidenai <30Dk81Yz>
2021-10-04 21:39:33 +0700

    add something

--- gendiff/engine/compare.py
+++ gendiff/engine/compare.py
@@ -11,16 +11,22 @@ FORMATS = {
 }
 
 
-def run():
+def parsing_args():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument("-f", "--format", choices=FORMATS.keys(), default="stylish", help='output format (default: "stylish")')
     parser.add_argument("-V", "--version", action="version", version="%(prog)s 0.10.2")
-    parsing(parser.parse_args())
+    args = parser.parse_args()
+    return args
+
+
+def generation(file1, file2, format_name='stylish'):
+    file1, file2 = parsing(file1, file2)
+    return FORMATS[format_name](get_diff(file1, file2))
 
 
 def parsing(args):
-    print(generate_diff(args.first_file, args.second_file))
+    return generate_diff(args.first_file, args.second_file))
 
 
