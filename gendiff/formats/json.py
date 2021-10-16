fd524fbd1b5194dd4f99e40ac767ef88fc01bbd6
Nidenai <30Dk81Yz>
2021-10-16 10:55:19 +0700

    update test 0.3

--- gendiff/formats/json.py
+++ gendiff/formats/json.py
@@ -2,4 +2,34 @@ import json
 
 
 def render(data):
-    return json.dumps(data, indent=2)
+    data = format(data)
+    data = json.dumps(data, indent=4)
+    return data
+
+
+def format(data):
+    result = {}
+    for item in data.keys():
+        status, key = item
+        if status == "changed":
+            old_value = check_value(data[item][0])
+            new_value = check_value(data[item][1])
+            value = ["updated", old_value, new_value]
+        else:
+            if status == "no change":
+                value = check_value(data[item])
+            else:
+                value = [status, check_value(data[item])]
+        result[key] = value
+    return result
+
+
+def is_child(value_type):
+    return value_type == "children"
+
+
+def check_value(value):
+    value_type, value = value
+    if is_child(value_type):
+        value = format(value)
+    return value
--- test/test_json.py
+++ test/test_json.py
@@ -11,6 +11,40 @@ def test_for_json():
     assert result == test_result
 
 
-test_for_json()
+def test_for_json_rec():
+    file_for_read = open('test/fixtures/test_rec_json.txt')
+    path_one = 'test/fixtures/examples/file1_rec.json'
+    path_two = 'test/fixtures/examples/file2_rec.json'
+    result = file_for_read.read()
+    test_result = generate_diff(path_one, path_two)
+    assert result == test_result
+
+
+def test_for_json_plain():
+    file_for_read = open('test/fixtures/test_json_plain.txt')
+    path_one = 'test/fixtures/examples/file1_rec.json'
+    path_two = 'test/fixtures/examples/file2_rec.json'
+    result = file_for_read.read()
+    test_result = generate_diff(path_one, path_two, format_name='plain')
+    assert result == test_result
+
+
+def test_for_json_json():
+    file_for_read = open('test/fixtures/test_json_json.txt')
+    path_one = 'test/fixtures/examples/file1_rec.json'
+    path_two = 'test/fixtures/examples/file2_rec.json'
+    result = file_for_read.read()
+    test_result = generate_diff(path_one, path_two, format_name='json')
+    assert result == test_result
+
+
+test_for_json_json()
+
+
+
+
+
+
+
 
 
