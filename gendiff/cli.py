194b0798ab871021fb84ca54546edab73e028908
Nidenai <30Dk81Yz>
2021-10-19 19:02:09 +0700

    fix linter

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,7 +6,10 @@ def generation():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument("-f", "--format", choices=FORMATS.keys(), default="stylish", help='output format (default: "stylish")')
+    parser.add_argument("-f", "--format",
+                        choices=FORMATS.keys(),
+                        default="stylish",
+                        help='output format (default: "stylish")')
     parser.add_argument("-V", "--version", action="version", version="%(prog)s 0.10.2")
     args = parser.parse_args()
     return args
