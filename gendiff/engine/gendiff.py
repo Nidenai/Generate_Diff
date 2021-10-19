92c3882e1eaa98fe28c2b0eb4b86da0dc252e552
Nidenai <30Dk81Yz>
2021-10-19 18:34:25 +0700

    refactoring 1.2

--- gendiff/engine/gendiff.py
+++ gendiff/engine/gendiff.py
@@ -4,9 +4,5 @@ from gendiff.cli import get_data
 
 
 def generate_diff(file1, file2, format_name='stylish'):
-    file1, file2 = parsing(file1, file2)
+    file1, file2 = generate_differenses(file1, file2)
     return FORMATS[format_name](get_data(file1, file2))
-
-
-def parsing(file1, file2):
-    return generate_differenses(file1, file2)
