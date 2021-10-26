cb7c48fca3c5eec17c6890b3d9c18bb8acd96cfc
Nidenai <30Dk81Yz>
2021-10-26 18:57:36 +0700

    refactoring 1.1

--- test/test_json.py
+++ test/test_json.py
@@ -14,7 +14,7 @@ def test_rec():
     file_for_read = open('test/fixtures/test_rec_json.txt')
     path_one = 'test/fixtures/examples/file1_rec.json'
     path_two = 'test/fixtures/examples/file2_rec.json'
-    result = file_for_read.read()шшшшшшш------
+    result = file_for_read.read()
     test_result = generate_diff(path_one, path_two)
     assert test_result == result
 
@@ -28,7 +28,10 @@ def test_plain():
     assert result == test_result
 
 
-def test_json():н
+def test_json():
     file_for_read = open('test/fixtures/test_json_json.txt')
     path_one = 'test/fixtures/examples/file1_rec.json'
-гшш
+    path_two = 'test/fixtures/examples/file2_rec.json'
+    result = file_for_read.read()
+    test_result = generate_diff(path_one, path_two, format_name='json')
+    assert result == test_result
