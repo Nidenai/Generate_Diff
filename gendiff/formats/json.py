bfed38972d2cff56171b8f304b6096803871df8e
Nidenai <30Dk81Yz>
2021-10-28 21:05:23 +0700

    refactoring 1.10

--- gendiff/formats/json.py
+++ gendiff/formats/json.py
@@ -10,20 +10,30 @@ def format(data):
     for item in data.keys():
         status, key = item
         if status == "changed":
-            old, new = check_value(data[item][0], data[item][1])
+            value_type_old, value_old = data[item][0]
+            if value_type_old == 'children':
+                old = format(value_old)
+            else:
+                old = value_old
+            value_type_new, value_new = data[item][1]
+            if value_type_new == 'children':
+                new = format(value_new)
+            else:
+                new = value_new
             value = ["updated", old, new]
         else:
             if status == "no change":
-                value = check_value(data[item])
+                value_type, value_value = data[item]
+                if value_type == "children":
+                    value = format(value_value)
+                else:
+                    value = value_value
             else:
-                value = [status, check_value(data[item])]
+                value_type, value_value = data[item]
+                if value_type == "children":
+                    middle_result = format(value_value)
+                else:
+                    middle_result = value_value
+                value = [status, middle_result]
         result[key] = value
     return result
-
-
-def check_value(*args):
-    for item in args:
-        value_type, value = item
-        if value_type == "children":
-            args = format(value)
-    return args
