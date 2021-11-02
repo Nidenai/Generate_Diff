152b67185bd1220f0587432279f26f5572e4c4be
Nidenai <30Dk81Yz>
2021-11-02 22:30:09 +0700

    refactoring 1.12.9

--- gendiff/formats/stylish.py
+++ gendiff/formats/stylish.py
@@ -43,5 +43,5 @@ def convert_value(data):
         for key, value in data.items():
             if isinstance(value, dict):
                 convert_value(value)
-            return '{}\n\t{}:{}\n\t{}'.format('{', key, value, '}')
+            return '{}\n  {}:{}\n{}'.format('{', key, value, '}')
     return data
