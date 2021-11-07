4f27f24b0b8c2e3b221725b6fdeea3f8f4111ca8
Nidenai <30Dk81Yz>
2021-11-07 20:40:10 +0700

    refactoring 1.12.23

--- gendiff/engine/differ.py
+++ gendiff/engine/differ.py
@@ -2,59 +2,28 @@ from gendiff.engine.parser import parse
 from gendiff.formats.var import FORMATS
 
 
-def get_diff(file1, file2):
+def get_diff(data1, data2):
     result = {}
-    keys = sorted(set(file1) | set(file2))
+    keys = sorted(data1.keys() | data2.keys())
     for key in keys:
-        status, value = check_keys(key, file1, file2)
+        v1, v2 = data1.get(key), data2.get(key)
+        if key in data1 and key not in data2:
+            status = "removed"
+            value = ["value", v1]
+        elif key in data2 and key not in data1:
+            status = "added"
+            value = ["value", v2]
+        elif v1 == v2:
+            status = "no change"
+            value = ["value", v1]
+        elif isinstance(v1, dict) and isinstance(v2, dict):
+            status = "no change"
+            value = ["children", get_diff(v1, v2)]
+        else:
+            status = "changed"
+            value = [["value", v1], ["value", v2]]
         result[(status, key)] = value
     return result
-
-
-def check_keys(key, before, after):
-    if key in before and key in after:
-        return key_remained(before[key], after[key])
-    elif key in before:
-        return key_in_one_file(before[key], "removed")
-    elif key in after:
-        return key_in_one_file(after[key], "added")
-
-
-def key_remained(before, after):
-    if before == after:
-        status = "no change"
-        if is_child(before):
-            value = ["children", get_diff(before, after)]
-        else:
-            value = ["value", before]
-    else:
-        status = "changed"
-        if is_child(before) and is_child(after):
-            status = "no change"
-            value = ["children", get_diff(before, after)]
-        elif is_child(before):
-            value = [
-                ["children", get_diff(before, before)],
-                ["value", after]
-            ]
-        elif is_child(after):
-            value = [
-                ["value", before],
-                ["children", get_diff(after, after)]
-            ]
-        else:
-            value = [["value", before], ["value", after]]
-    return status, value
-
-
-def key_in_one_file(data, status):
-    if is_child(data):
-        return status, ["children", get_diff(data, data)]
-    return status, ["value", data]
-
-
-def is_child(data):
-    return isinstance(data, dict)
 
 
 def generate_diff(file1, file2, format_name='stylish'):
--- gendiff/formats/stylish.py
+++ gendiff/formats/stylish.py
@@ -29,8 +29,8 @@ def to_string(data, lvl=0):
     for key, value in data.items():
         if isinstance(value, dict):
             value = to_string(value, lvl + 1)
-        result += f"{'   ' * lvl}{key}: {value}\n"
-    result += f"{'   ' * lvl}}}"
+        result += f"{'    ' * lvl}{key}: {value}\n"
+    result += f"{'    ' * lvl}}}"
     return edit_message(result)
 
 
@@ -44,6 +44,6 @@ def convert_value(v):
 
     result = {}
     for key, value in v.items():
-        new_key = '  {}'.format(key)
+        new_key = '    {}'.format(key)
         result[new_key] = convert_value(value)
     return result
