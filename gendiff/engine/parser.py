0038989a9b8e14b4fc158d24b459d313a264f369
Nidenai <30Dk81Yz>
2021-11-10 19:00:10 +0700

    refactoring 1.12.28

--- gendiff/engine/parser.py
+++ gendiff/engine/parser.py
@@ -3,19 +3,19 @@ import yaml
 
 
 def parse(file1, file2):
-    if is_json(file1, file2):
+    if is_json(file1) and is_json(file2):
         arg1 = json.load(open(file1))
         arg2 = json.load(open(file2))
-    elif is_yaml(file1, file2):
+    elif is_yaml(file1) and is_yaml(file2):
         arg1 = yaml.load(open(file1), Loader=yaml.FullLoader)
         arg2 = yaml.load(open(file2), Loader=yaml.FullLoader)
 
     return arg1, arg2
 
 
-def is_json(file1, file2):
-    return file1.endswith(".json") and file2.endswith(".json")
+def is_json(file1):
+    return file1.endswith(".json")
 
 
-def is_yaml(file1, file2):
-    return file1.endswith((".yaml", ".yml")) and file2.endswith((".yaml", ".yml"))
+def is_yaml(file1):
+    return file1.endswith(".yaml", ".yml")
